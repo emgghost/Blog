fdf4bd87e346c6dde9b15cf1b2314eb3823f893e
emg <emg.programer@gmail.com>
2025-07-21 16:26:37 +0330

    feat(Blog.Infrastructure): add SliderImageUrl property to Post entity
    - Add SliderImageUrl property to Post entity in AppDbContextModelSnapshot
    - The new property is of type string and has a max length of 200 characters

---
+++ Backend/Blog.Infrastructure/Migrations/20250721125622_Add Slider Url.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSliderUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SliderImageUrl",
+                table: "BlogPosts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SliderImageUrl",
+                table: "BlogPosts");
+        }
+    }
+}
